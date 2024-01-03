e4623104b81ab884e7cd813ecf827f1c41a83622
zblanco <zkwhited@gmail.com>
2024-01-03 12:03:38 -0700

    init

---
+++ lib/cozonomono.ex
@@ -0,0 +1,18 @@
+defmodule Cozonomono do
+  @moduledoc """
+  Documentation for `Cozonomono`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cozonomono.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cozonomono_test.exs
@@ -0,0 +1,8 @@
+defmodule CozonomonoTest do
+  use ExUnit.Case
+  doctest Cozonomono
+
+  test "greets the world" do
+    assert Cozonomono.hello() == :world
+  end
+end
