ac7a97866f5f109e26aaae75b57818304b882893
Christopher Grainger <chris@amplified.ai>
2024-02-02 13:25:14 +0100

    Test more types

--- test/cozonomono_test.exs
+++ test/cozonomono_test.exs
@@ -18,4 +18,16 @@ defmodule CozonomonoTest do
                 "rows" => [["hello", "world", "Cozo!"]]
               }}
   end
+
+  test "can handle many data types" do
+    {:ok, instance} = Cozonomono.new()
+
+    assert Cozonomono.simple_query(instance, "?[] <- [['hello', 100, 100.0, [1, 2, 3]]]") ==
+             {:ok,
+              %{
+                "headers" => ["_0", "_1", "_2", "_3"],
+                "next" => nil,
+                "rows" => [["hello", 100, 100.0, [1, 2, 3]]]
+              }}
+  end
 end
