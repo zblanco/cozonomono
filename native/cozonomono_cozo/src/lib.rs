5b885e337ae1b55aa24dea8b982229e8d215fa85
Christopher Grainger <chris@amplified.ai>
2024-02-02 10:52:41 +0100

    Add thiserror

--- native/cozonomono_cozo/src/lib.rs
+++ native/cozonomono_cozo/src/lib.rs
@@ -1,8 +1,9 @@
-// use cozo::*;
+mod error;
 
 #[rustler::nif]
 fn add(a: i64, b: i64) -> i64 {
     a + b
+pub use error::ExError;
 }
 
 // specify all imported functions from modules we want exposed over NIF
