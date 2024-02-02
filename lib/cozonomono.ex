617818d3e0f2be4d1e11c4906c37de044ad13efd
Christopher Grainger <chris@amplified.ai>
2024-02-02 16:32:51 +0100

    Enable decoding params

--- lib/cozonomono.ex
+++ lib/cozonomono.ex
@@ -7,6 +7,11 @@ defmodule Cozonomono do
   alias Cozonomono.Native
 
   @type engine :: :mem | :sqlite | :rocksb
+  @type named_rows :: %{
+          String.t() => [String.t()],
+          String.t() => named_rows(),
+          String.t() => [list()]
+        }
 
   @doc """
   Creates a new Cozo database instance.
@@ -15,7 +20,25 @@ defmodule Cozonomono do
   def new(engine \\ :mem, path \\ ""),
     do: engine |> Atom.to_string() |> Native.create_instance(path)
 
-  @spec simple_query(instance :: Instance.t(), query :: String.t()) ::
-          {:ok, [map()]} | {:error, term()}
-  def simple_query(instance, query), do: Native.run_default(instance, query)
+  @default_query_opts [params: nil, immutable?: false]
+
+  @doc """
+  Runs a query against the given db instance.
+
+  Params must be passed as a map with string keys.
+  """
+  @spec query(
+          instance :: Instance.t(),
+          query :: String.t(),
+          opts :: Keyword.t()
+        ) :: {:ok, named_rows()} | {:error, term()}
+  def query(instance, query, opts \\ []) do
+    opts = Keyword.validate!(opts, @default_query_opts)
+
+    if Keyword.equal?(opts, @default_query_opts) do
+      Native.run_default(instance, query)
+    else
+      Native.run_script(instance, query, opts[:params], opts[:immutable?])
+    end
+  end
 end
